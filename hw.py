45d825e74bc30513e3ef4de874573b9a46f2a563
everling <everling@kth.se>
2016-09-28 11:58:18 +0200

    linear kernel

--- hw.py
+++ hw.py
@@ -3,3 +3,13 @@ from cvxopt.base import matrix
 import numpy, pylab, random, math
 
 print ("This line will be printed.")
+
+
+def linear_kernel(x,y):
+	return numpy.dot(x,y) + 1
+	
+x = numpy.array([1,2,3])
+y = numpy.array([2,2,2])
+print (linear_kernel(x,y))
+
+
